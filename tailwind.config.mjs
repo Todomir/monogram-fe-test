ca4cdb0110e152c8b5bfa7e246aae6d0840c79c6
Abner Rodrigues <abnerluis1001@gmail.com>
2023-12-12 21:28:18 -0300

    feat: add float animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			animation: {
+				float: "float 5s infinite ease-in-out",
+			},
+			keyframes: {
+				float: {
+					"0%, 100%": { transform: "translateY(0)" },
+					"50%": { transform: "translateY(-4%)" },
+				},
+			},
 			backgroundImage: {
-				'hero-gradient': 'url("./src/assets/hero-text-gradient.png")'
-			}
-		}
+				"hero-gradient": 'url("./src/assets/hero-text-gradient.png")',
+			},
+		},
 	},
-	plugins: []
-}
+	plugins: [],
+};
