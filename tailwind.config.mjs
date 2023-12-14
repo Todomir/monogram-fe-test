de8141743f9ad9628ff484ccb0ae35aee724b8ee
Abner Rodrigues <abnerluis1001@gmail.com>
2023-12-14 10:21:43 -0300

    feat: add `motion` package

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,31 @@ export default {
 		extend: {
 			animation: {
 				float: "float 5s infinite ease-in-out",
+				"fade-in-down": "fade-in-down 0.6s ease-in-out forwards",
+				"fade-in": "fade-in 0.6s ease-in forwards",
 			},
 			keyframes: {
 				float: {
 					"0%, 100%": { transform: "translateY(0)" },
 					"50%": { transform: "translateY(-4%)" },
+				},
+				"fade-in-down": {
+					"0%": {
+						opacity: "0",
+						transform: "translateY(-10%)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+				"fade-in": {
+					"0%": {
+						opacity: "0",
+					},
+					"100%": {
+						opacity: "1",
+					},
 				},
 			},
 			backgroundImage: {
